843d7dec0c57945a1f271e603314184b00ec3758
Itanor Strapazzon <itanor@gmail.com>
2022-01-24 23:07:30 -0300

    post user data/generate sha from user id

--- pass-generator/src/main.rs
+++ pass-generator/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use log::{info};
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
+use sha256::digest;
+
+#[macro_use] extern crate rocket;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    id: String,
+    name: String,
+    hash: String,
+    served: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct WaitingUser {
+    id: String,
+    name: String,
+}
+
+#[post("/generate-hash", format = "json", data = "<user>")]
+fn generate_hash(user: Json<WaitingUser>) {
+    println!("{:?}", user); 
+
+    let sha = digest(&user.id);
+    info!("sha = {}", sha);
+
+    // send to redis...
+    // send to rabiitmq...
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![
+            generate_hash
+        ])
 }
